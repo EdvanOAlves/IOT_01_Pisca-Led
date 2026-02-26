b2c96472a66068bc20ccf9edae70b8d7484c1e6d
EDVAN ALVES DE OLIVEIRA <25132698@CFP127ALUNO.LOCAL>
2026-02-26 10:29:52 -0300

    feat: change delay time and output pin

--- sketch.ino
+++ sketch.ino
@@ -2,12 +2,12 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("Hello, ESP32!");
-  pinMode(18, OUTPUT);
+  pinMode(21, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(18, HIGH);
-  delay(500); // Wait for 500 millisecond(s)
-  digitalWrite(18, LOW);
-  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(21, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(21, LOW);
+  delay(200); // Wait for 500 millisecond(s)
 }
